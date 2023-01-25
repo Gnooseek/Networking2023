a22af0459f11a99b0c05bf7818855fcbf59c31b3
Philip Supruniuk <375298425457@bk.ru>
2023-01-25 15:49:34 +0300

    added project;

---
+++ Networking2023/Networking2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Networking2023
+//
+//  Created by Fill on 25.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
